429e25f4745d1bd82d68d3eee35bbd7153394a10
Steve Ernstberger <>
2020-05-03 15:05:25 -0400

    removed more files

--- New Folder With Items/components/Button/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const StyledButton: import("styled-components").StyledComponent<import("@material-ui/core").ExtendButtonBase<import("@material-ui/core/Button").ButtonTypeMap<{}, "button">>, any, {}, never>;
-export default StyledButton;
--- New Folder With Items/components/Icons/StudentsLine.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: import("@material-ui/core/OverridableComponent").OverridableComponent<import("@material-ui/core").SvgIconTypeMap<{}, "svg">>;
-export default _default;

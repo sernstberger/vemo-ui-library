caf01739835b2ea3b052a12a32a900b34ec44959
Steve Ernstberger <>
2020-05-03 22:18:09 -0400

    theme updates

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -4,119 +4,121 @@ import { createMuiTheme } from '@material-ui/core/styles'
 const vemoBlue = '#0073D1'
 
 // A custom theme for this app
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: vemoBlue
-    },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    background: {
-      default: '#fff'
-    }
+
+const palette = {
+  primary: {
+    main: vemoBlue
   },
-
-  overrides: {
-    MuiInputBase: {
-      root: {
-        borderRadius: 4,
-        position: 'relative',
-        // backgroundColor: theme.palette.common.white,
-        border: '2px solid #ced4da',
-        fontSize: '1rem',
-        fontWeight: 600,
-        // transition: theme.transitions.create(['border-color', 'box-shadow']),
-        width: '100%',
-
-        'label + &': {
-          // marginTop: theme.spacing(2.25)
-        },
-
-        '&$focused': {
-          // borderColor: theme.palette.primary.main
-        },
-        '&$disabled': {
-          // backgroundColor: theme.palette.background.default
-        },
-        '&$error': {
-          // borderColor: theme.palette.error.main
-        }
-      }
-    }
+  secondary: {
+    main: '#19857b'
   },
-  props: {
-    MuiCard: {
-      elevation: 0
-    },
-    MuiFormControl: {
-      fullWidth: true
-    },
-    MuiMenu: {
-      getContentAnchorEl: null,
-      anchorOrigin: {
-        vertical: 'bottom',
-        horizontal: 'left'
-      }
-    },
-    MuiTextField: {
-      fullWidth: true,
-      variant: 'standard',
-      margin: 'normal'
-    },
-    MuiInput: {
-      disableUnderline: true
-    },
-    MuiInputLabel: {
-      shrink: true
-    },
-    MuiLink: {
-      underline: 'always'
-    },
-    MuiSelect: {
-      native: true
-      // IconComponent: () => (
-      //   <div
-      //     style={{
-      //       borderLeft: `1px solid #CCC`,
-      //       pointerEvents: 'none',
-      //       display: 'flex',
-      //       alignItems: 'center',
-      //       position: 'absolute',
-      //       paddingLeft: theme.spacing(1.5),
-      //       marginLeft: theme.spacing(1.5),
-      //       right: theme.spacing(1.5),
-      //       height: '75%',
-      //       width: theme.spacing(4.5)
-      //     }}
-      //   >
-      //     <Icon icon="arrow-down" />
-      //   </div>
-      // )
-    },
-    MuiSlider: {
-      color: 'primary'
-      // ThumbComponent: (props) => (
-      //   <span {...props}>
-      //     <Icon icon="slider-thumb" size="small" />
-      //   </span>
-      // )
-    },
+  error: {
+    main: red.A400
+  },
+  background: {
+    default: '#fff'
+  }
+}
 
-    MuiSnackbar: {
-      anchorOrigin: {
-        vertical: 'top',
-        horizontal: 'center'
-      }
-    },
+const overrides = {
+  MuiInputBase: {
+    root: {
+      borderRadius: 4,
+      position: 'relative',
+      // backgroundColor: theme.palette.common.white,
+      border: '2px solid #ced4da',
+      fontSize: '1rem',
+      fontWeight: 600,
+      // transition: theme.transitions.create(['border-color', 'box-shadow']),
+      width: '100%',
 
-    MuiTooltip: {
-      disableTouchListener: true
+      'label + &': {
+        // marginTop: theme.spacing(2.25)
+      },
+
+      '&$focused': {
+        borderColor: palette.primary.main
+      },
+      '&$disabled': {
+        backgroundColor: palette.background.default
+      },
+      '&$error': {
+        borderColor: palette.error.main
+      }
     }
   }
-})
+}
+
+const props = {
+  MuiCard: {
+    elevation: 0
+  },
+  MuiFormControl: {
+    fullWidth: true
+  },
+  MuiMenu: {
+    getContentAnchorEl: null,
+    anchorOrigin: {
+      vertical: 'bottom',
+      horizontal: 'left'
+    }
+  },
+  MuiTextField: {
+    fullWidth: true,
+    variant: 'standard',
+    margin: 'normal'
+  },
+  MuiInput: {
+    disableUnderline: true
+  },
+  MuiInputLabel: {
+    shrink: true
+  },
+  MuiLink: {
+    underline: 'always'
+  },
+  MuiSelect: {
+    native: true
+    // IconComponent: () => (
+    //   <div
+    //     style={{
+    //       borderLeft: `1px solid #CCC`,
+    //       pointerEvents: 'none',
+    //       display: 'flex',
+    //       alignItems: 'center',
+    //       position: 'absolute',
+    //       paddingLeft: theme.spacing(1.5),
+    //       marginLeft: theme.spacing(1.5),
+    //       right: theme.spacing(1.5),
+    //       height: '75%',
+    //       width: theme.spacing(4.5)
+    //     }}
+    //   >
+    //     <Icon icon="arrow-down" />
+    //   </div>
+    // )
+  },
+  MuiSlider: {
+    color: 'primary'
+    // ThumbComponent: (props) => (
+    //   <span {...props}>
+    //     <Icon icon="slider-thumb" size="small" />
+    //   </span>
+    // )
+  },
+
+  MuiSnackbar: {
+    anchorOrigin: {
+      vertical: 'top',
+      horizontal: 'center'
+    }
+  },
+
+  MuiTooltip: {
+    disableTouchListener: true
+  }
+}
+
+const theme = createMuiTheme({ palette, overrides, props })
 
 export default theme

385853fb48b0bd86ac3279a329803ecd3489f157
Steve Ernstberger <>
2020-05-03 23:23:21 -0400

    added some knobs

--- src/components/Form/ValidatedInputBase/index.stories.tsx
+++ src/components/Form/ValidatedInputBase/index.stories.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 // import { action } from '@storybook/addon-actions'
 import ValidatedInputBase from './index'
-// import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
 
 export default {
   title: 'Validated Input',
   component: ValidatedInputBase,
-  // decorators: [withKnobs]
+  decorators: [withKnobs]
 }
 
 export const ValidatedInputBaseStory = () => (
   <ValidatedInputBase
     label="Text Input"
-    required
+    required={boolean('Required', true)}
+    error={boolean('Error', false)}
+    helperText="Helper text goes here"
   />
 )

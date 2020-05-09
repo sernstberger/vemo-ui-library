07a25ddfe15c3fac52f0d7bccf5cb2f5e24225d9
Steve Ernstberger <>
2020-05-09 08:18:27 -0400

    drawer looks much better

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -12,27 +12,29 @@ const PageLayout = (props: PageLayoutProps) => {
   const { children, title } = props
   const classes = styles()
   return (
-    <div style={{ backgroundColor: 'orange', display: 'flex' }}>
-      <Drawer
-        variant="permanent"
-        anchor="left"
-        classes={{
-          paper: classes.drawerPaper
-        }}
-        open
-      >
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Purdue_Boilermakers_logo.svg/1200px-Purdue_Boilermakers_logo.svg.png"
-          alt="logo"
-          style={{ maxWidth: 100, padding: '0 30px' }}
-        />
-        <nav>lakdsjflkj</nav>
-        {/* {navigation} */}
-        <div>
-          <Divider />
-          <Legal />
-        </div>
-      </Drawer>
+    <div style={{ display: 'flex' }}>
+      <Hidden xsDown>
+        <Drawer
+          variant="permanent"
+          anchor="left"
+          classes={{
+            paper: classes.drawerPaper
+          }}
+          open
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Purdue_Boilermakers_logo.svg/1200px-Purdue_Boilermakers_logo.svg.png"
+            alt="logo"
+            style={{ maxWidth: 100, padding: '0 30px' }}
+          />
+          <nav>lakdsjflkj</nav>
+          {/* {navigation} */}
+          <div>
+            <Divider />
+            <Legal />
+          </div>
+        </Drawer>
+      </Hidden>
 
       <div>
         <div style={{ backgroundColor: 'purple' }}>
@@ -52,7 +54,16 @@ const PageLayout = (props: PageLayoutProps) => {
           </AppBar>
         </div>
 
-        <div style={{ backgroundColor: 'white', padding: 32 }}>{children}</div>
+        <div className={classes.LayoutContent}>
+          {/* <Snackbar
+            open={snackbar.open}
+            status={snackbar.status}
+            message={snackbar.message}
+            autoHideDuration={6000}
+            onClose={handleSnackbarClose}
+          /> */}
+          {children}
+        </div>
       </div>
     </div>
   )
--- src/components/PageLayout/styles.ts
+++ src/components/PageLayout/styles.ts
@@ -6,7 +6,7 @@ const styles = makeStyles((theme: Theme) => ({
     backgroundColor: 'transparent',
     display: 'flex',
     minHeight: theme.spacing(8),
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('sm')]: {
       marginLeft: theme.drawerWidth,
       width: `calc(100% - ${theme.drawerWidth}px)`
     }
@@ -16,11 +16,19 @@ const styles = makeStyles((theme: Theme) => ({
     backgroundColor: '#FFF',
     borderRadius: '0 30px 0 0',
     height: '100vh',
-    borderRight: '1px solid red',
-    borderTop: '1px solid red',
+    borderRight: `1px solid ${theme.palette.divider}`,
+    borderTop: `1px solid ${theme.palette.divider}`,
     overflowX: 'hidden',
     paddingTop: 30,
     width: theme.drawerWidth
+  },
+
+  LayoutContent: {
+    backgroundColor: 'white',
+    padding: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      paddingLeft: `calc(${theme.drawerWidth}px + ${theme.spacing(4)}px)`
+    }
   }
 }))
 

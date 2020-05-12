c0969734eb0e656c3386a6c7316970c710ca3489
Steve Ernstberger <>
2020-05-12 19:11:36 -0400

    added progress

---
+++ src/components/Progress/styles.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/styles'
+import { Theme } from '@material-ui/core'
+
+const styles = makeStyles((theme: Theme) => ({
+  LinearProgress: {
+    marginBottom: theme.spacing(1)
+  }
+}))
+
+export default styles

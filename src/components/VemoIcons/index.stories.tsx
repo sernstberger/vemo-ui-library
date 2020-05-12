fdfc70e877f76b1b6a99f68f9c4588db9356b166
Steve Ernstberger <>
2020-05-12 13:54:49 -0400

    All the icons are rendering

--- src/components/VemoIcons/index.stories.tsx
+++ src/components/VemoIcons/index.stories.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
 import Foo from './Foo'
+import iconsArray from './build/iconsArray'
+import { Grid } from '@material-ui/core'
 
 export default {
-  title: 'Icons',
+  title: 'Icons'
   // component: Icon
 }
 
+console.log('!!!!', iconsArray)
+
 export const IconsStory = () => (
-  <div style={{ backgroundColor: 'red', display: 'flex' }}>
-    <div><Foo name="Help" /></div>
-  </div>
+  <Grid container spacing={2}>
+    {iconsArray.map((icon: any) => {
+      return (
+        <Grid item xs={3}>
+          <div style={{ backgroundColor: 'red' }}>
+            <Foo name={icon.name} />
+            {icon.name}
+          </div>
+        </Grid>
+      )
+    })}
+  </Grid>
 )

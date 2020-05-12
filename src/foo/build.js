07bfb04a3748369ac0ccebf208a4e0a19d14f106
Steve Ernstberger <>
2020-05-11 22:12:29 -0400

    it builds an array of things

--- src/foo/build.js
+++ src/foo/build.js
@@ -1,9 +1,11 @@
-const fs = require('fs-extra');
+const fs = require('fs-extra')
 const path = require('path')
 
-const BUILD_PATH = path.join(__dirname, '..', 'foo');
-const SOMETHING = path.join(BUILD_PATH, 'build')
-console.log('!!!', BUILD_PATH)
-console.log('???', SOMETHING)
+const array = ['thing', 'blah']
 
-fs.outputFile(SOMETHING, '<div>alskdfj</div>');
+const BUILD_PATH = path.join(__dirname, '..', 'foo')
+
+array.map(yep => {
+  const OUTPUT_PATH = path.join(BUILD_PATH, `bar/${yep}.js`)
+  fs.outputFile(OUTPUT_PATH, '<div>alskdfj</div>')
+})

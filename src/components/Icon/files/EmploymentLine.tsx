48f56d2f75f2591f1d6bc3f5be63018b5c1e4c13
Steve Ernstberger <>
2020-05-12 18:41:31 -0400

    added all the icons

---
+++ src/components/Icon/files/EmploymentLine.tsx
@@ -0,0 +1,16 @@
+
+// ************************************************************ //
+// ** DO NOT directly edit this file - it is auto-generated! ** //
+// ************************************************************ //
+
+import React from 'react'
+import BuildIcon, { IconProps } from '../src/BuildIcon'
+
+const EmploymentLine = (props: IconProps) => (
+  <BuildIcon
+    {...props}
+    path="M10.3,12c0.3-0.6,1-1,1.7-1s1.4,0.4,1.7,1H19v-2c0-0.6-0.4-1-1-1H6c-0.6,0-1,0.4-1,1v2H10.3 L10.3,12z M10.3,14H5v3c0,0.6,0.4,1,1,1h12c0.6,0,1-0.4,1-1v-3h-5.3c-0.3,0.6-1,1-1.7,1S10.6,14.6,10.3,14L10.3,14z M5,7h14 c1.1,0,2,0.9,2,2v9c0,1.1-0.9,2-2,2H5c-1.1,0-2-0.9-2-2V9C3,7.9,3.9,7,5,7z M8,6H6V5c0-1.1,0.9-2,2-2h8c1.1,0,2,0.9,2,2v1h-2V5H8V6 z"
+  />
+)
+
+export default EmploymentLine
---
+++ src/components/Icon/files/StudentFill.tsx
@@ -0,0 +1,16 @@
+
+// ************************************************************ //
+// ** DO NOT directly edit this file - it is auto-generated! ** //
+// ************************************************************ //
+
+import React from 'react'
+import BuildIcon, { IconProps } from '../src/BuildIcon'
+
+const StudentFill = (props: IconProps) => (
+  <BuildIcon
+    {...props}
+    path="M12.1,12.3c0.7,0,1.8,0.4,3.5,1.3c1.5,0.8,2,2.6,1.2,4.1c-0.5,1-1.5,1.6-2.6,1.6 c-0.7,0-1.4,0-2.1,0c-0.8,0-1.5,0-2.2,0c-1.7,0-3-1.4-2.9-3.1c0-1.1,0.6-2.1,1.6-2.6C10.3,12.7,11.5,12.3,12.1,12.3z M12.1,11 c-1.7,0-3-1.3-3-3s1.3-3,3-3s3,1.3,3,3S13.8,11,12.1,11z"
+  />
+)
+
+export default StudentFill

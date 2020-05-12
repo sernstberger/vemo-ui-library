4e3192b5cb94de5ff1c4333c404e2935a32a62cc
Steve Ernstberger <>
2020-05-12 08:29:59 -0400

    mustache is creating js files nicely.

--- src/foo/build.js
+++ src/foo/build.js
@@ -1,12 +1,29 @@
 const fs = require('fs-extra')
 const path = require('path')
 const component = require('./component')
+const Mustache = require('mustache')
+
+// const fileString = Mustache.render(template, {
+//   paths,
+//   componentName: getComponentName(destPath),
+// });
+
+var template = `import React from 'react'
+
+const Component = () => {
+  return (
+    <div>alksjflaksjf</div>
+  )
+}
+
+export default Component`
+var rendered = Mustache.render(template, { name: 'Luke' })
 
 const array = ['thing', 'blah']
 
 const BUILD_PATH = path.join(__dirname, '..', 'foo')
 
 array.map(yep => {
-  const OUTPUT_PATH = path.join(BUILD_PATH, `bar/${yep}.js`)
-  fs.outputFile(OUTPUT_PATH, component)
+  const OUTPUT_FILE = path.join(BUILD_PATH, `finished/${yep}.js`)
+  fs.outputFile(OUTPUT_FILE, rendered)
 })
--- src/foo/finished/blah.js
+++ src/foo/finished/blah.js
@@ -1,4 +1,4 @@
-`import React from 'react'
+import React from 'react'
 
 const Component = () => {
   return (
@@ -6,4 +6,4 @@ const Component = () => {
   )
 }
 
-export default Component`
+export default Component

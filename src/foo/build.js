13b8f89a08bbb7e08e78c9fed4fefdc87ed846cf
Steve Ernstberger <>
2020-05-11 23:03:48 -0400

    kinda in the same spot

--- src/foo/build.js
+++ src/foo/build.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra')
 const path = require('path')
+const component = require('./component')
 
 const array = ['thing', 'blah']
 
@@ -7,5 +8,5 @@ const BUILD_PATH = path.join(__dirname, '..', 'foo')
 
 array.map(yep => {
   const OUTPUT_PATH = path.join(BUILD_PATH, `bar/${yep}.js`)
-  fs.outputFile(OUTPUT_PATH, '<div>alskdfj</div>')
+  fs.outputFile(OUTPUT_PATH, component)
 })
---
+++ src/foo/component.js
@@ -0,0 +1,11 @@
+'fooooooooo'
+
+// import React from 'react'
+
+// const Component = () => {
+//   return (
+//     <div>alksjflaksjf</div>
+//   )
+// }
+
+// export default Component

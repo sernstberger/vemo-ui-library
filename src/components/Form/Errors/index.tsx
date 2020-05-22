2b6294c8360058b2211d22d8e5fde55cd2fdaf0c
Steve Ernstberger <>
2020-05-21 20:12:15 -0400

    let's get recursive

--- src/components/Form/Errors/index.tsx
+++ src/components/Form/Errors/index.tsx
@@ -22,25 +22,45 @@ const Errors = (props: ErrorsProps) => {
 
   let finalArray: ErrorProps[] = []
 
-  const foo = Object.entries(errors)
+  // const foo = Object.entries(errors)
 
-  foo.map((baz: any) => {
+  // foo.map((baz: any) => {
     
-    if (typeof baz[1] === 'object') {
-      baz[1].map((bat: any) => {
-        const hit = Object.entries(bat)
-        hit.map((ball: any) => {
-          const ugh = {field: ball[0], message: ball[1]}
-          finalArray = [...finalArray, ugh]
-        })
+  //   if (typeof baz[1] === 'object') {
+  //     baz[1].map((bat: any) => {
+  //       const hit = Object.entries(bat)
+  //       hit.map((ball: any) => {
+  //         const ugh = {field: ball[0], message: ball[1]}
+  //         finalArray = [...finalArray, ugh]
+  //       })
+  //     })
+  //   } else {
+  //     const thing = {field: baz[0], message: baz[1]}
+  //     finalArray = [...finalArray, thing]
+  //   }
+  // })
 
-        // console.log('*****', hit)
-      })
-    } else {
-      const thing = {field: baz[0], message: baz[1]}
-      finalArray = [...finalArray, thing]
-    }
-  })
+
+
+
+  const createErrorArray = (array: any) => {
+
+
+    Object.entries(array).map((baz: any) => {
+    
+      // if (typeof baz[1] === 'object') {
+      if (Array.isArray(baz[1])) {
+        baz[1].map((bat: any) => {
+          createErrorArray(bat)
+        })
+      } else {
+        const thing = {field: baz[0], message: baz[1]}
+        finalArray = [...finalArray, thing]
+      }
+    })
+  }
+
+  createErrorArray(errors)
 
 
 // console.log('^^^^^^^', finalArray)

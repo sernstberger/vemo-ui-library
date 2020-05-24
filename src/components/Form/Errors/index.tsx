b38e7db3d54211a5f9533deffb15914dc3580631
Steve Ernstberger <>
2020-05-24 00:46:20 -0400

    working

--- src/components/Form/Errors/index.tsx
+++ src/components/Form/Errors/index.tsx
@@ -1,47 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Snackbar, { SnackbarMessageProps } from '../../Snackbar'
 import { useFormikContext, getIn } from 'formik'
 
-const Errors = (props: ErrorsProps) => {
+const Errors = (props: any) => {
   const { errors, values, touched, submitCount } = useFormikContext()
 
-  let messagesArray: any = []
-  const createErrorArray = (obj: any) => {
-    Object.entries(obj).map((errorObj: any) => {
-      if (Array.isArray(errorObj[1])) {
-        errorObj[1].map((ErrorObjObj: any, index: number) => {
-          Object.entries(ErrorObjObj).map((blah: any) => {
-            const foo = errorObj[0]
-            messagesArray = [...messagesArray, `${foo}[${index}].${blah[0]}`]
+  const [snackbarMessages, setSnackbarMessages] = useState([])
+
+  useEffect(() => {
+    let messagesArray: any = []
+    const createErrorArray = (obj: any) => {
+      Object.entries(obj).map((errorObj: any) => {
+        if (Array.isArray(errorObj[1])) {
+          errorObj[1].map((ErrorObjObj: any, index: number) => {
+            Object.entries(ErrorObjObj).map((blah: any) => {
+              const foo = errorObj[0]
+              messagesArray = [...messagesArray, `${foo}[${index}].${blah[0]}`]
+            })
           })
-        })
-      } else {
-        messagesArray = [...messagesArray, errorObj[0]]
+        } else {
+          messagesArray = [...messagesArray, errorObj[0]]
+        }
+      })
+    }
+
+    createErrorArray(values)
+
+    const formattedMessages = messagesArray.map((obj: any) => {
+      const error = getIn(errors, obj)
+      const touch = getIn(touched, obj)
+      const errorMessageText = touch && error ? error : null
+
+      if (errorMessageText !== null) {
+        return {
+          text: errorMessageText,
+          onClick: () => {
+            document.getElementById(obj)!.focus()
+          }
+        } as SnackbarMessageProps
       }
     })
-  }
 
-  createErrorArray(values)
+    const filteredMessages = formattedMessages.filter(
+      (item: any) => item !== undefined
+    )
 
-  const formattedMessages = messagesArray.map((obj: any) => {
-    const error = getIn(errors, obj)
-    const touch = getIn(touched, obj)
-    const foo = touch && error ? error : null
+    setSnackbarMessages(filteredMessages)
+  }, [submitCount])
 
-    if (foo !== null) {
-      return {
-        text: foo,
-        onClick: () => {
-          document.getElementById(obj)!.focus()
-        }
-      } as SnackbarMessageProps
-    }
-  })
-
-  const filteredMessages = formattedMessages.filter((item: any) => item !== undefined)
-
-  if (filteredMessages.length && submitCount > 0) {
-    return <Snackbar open status="error" messages={filteredMessages} />
+  if (snackbarMessages.length && submitCount > 0) {
+    return (
+      <Snackbar
+        open
+        status="error"
+        title="Something is not right..."
+        messages={snackbarMessages}
+      />
+    )
   }
 
   return null
